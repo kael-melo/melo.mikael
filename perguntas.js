5f659be8426e6ca4b6db1c8c93075cd305040285
kael-melo <161338104+kael-melo@users.noreply.github.com>
2024-10-21 11:59:36 -0300

    mikaelmelo

--- perguntas.js
+++ perguntas.js
@@ -1,6 +1,26 @@
-cicarao(
-    ´quem criou o volei´
-    ´quis sao os maiores jogadores vole´
-    ´qual foi 0 primeiro time a sé campeao olimopico
+criaCartao(
+    'Quem criou o volei?',
+    'Quis sao os maiores jogadores vole?',
+    'Qual foi 0 primeiro time a sé campeao olimopico?'
     
+)
+    
+criaCartao(
+    'Quem foi o maior professor de geografia?',
+    'Qual o maior pais do mundo?',
+    'A capital da frança é em Paris'
+
+)
+
+criaCartao(
+    'Quem era o homem chamado de pai da matematica?',
+    'O que Pitágoras criou?',
+    'Quem são 5 gênio da matematica?'
+
+)
+criaCartao(
+    'Onde o ingle surgiu?',
+    'Em qual pais o ingles e a primeira lingua ?',
+    'Qual e a lingua mais falada no mundo?'
+
 )
